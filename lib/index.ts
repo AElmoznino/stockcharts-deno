1fed17cff12a344fadbd96f5905c32c330e00e73
André Elmoznino Laufer <andre.elmoznino@valtech.com>
2020-05-15 13:35:46 +0200

    chore: add types for index.ts, prettier logs

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,24 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import {
+  bold,
+  green,
+} from "https://deno.land/std@0.51.0/fmt/colors.ts";
+import {
+  Application,
+  RouterContext,
+  Router,
+} from "https://deno.land/x/oak/mod.ts";
 import { getDailyStockQuotes, getStocksByKeywords } from "./routes/stocks.ts";
 import { getCurrencies } from "./routes/fx.ts";
+
+const PORT = 3000;
 const router = new Router();
+const app = new Application();
 
 router
-  .get("/", (context) => {
+  .get("/", (context: RouterContext) => {
     context.response.body = "Hello world!";
   })
-  .get("/stocks/:symbol", async (context) => {
+  .get("/stocks/:symbol", async (context: RouterContext) => {
     if (context.params && context.params.symbol) {
       const quotes = await getDailyStockQuotes(context.params.symbol);
       context.response.body = quotes;
@@ -15,7 +26,7 @@ router
       context.throw(404);
     }
   })
-  .get("/search/:keywords", async (context) => {
+  .get("/search/:keywords", async (context: RouterContext) => {
     if (context.params && context.params.keywords) {
       const stocks = await getStocksByKeywords(context.params.keywords);
       context.response.body = stocks;
@@ -23,7 +34,7 @@ router
       context.throw(404);
     }
   })
-  .get("/fx/:from/:to", async (context) => {
+  .get("/fx/:from/:to", async (context: RouterContext) => {
     if (context.params && context.params.from && context.params.to) {
       const fx = await getCurrencies(context.params.from, context.params.to);
       context.response.body = fx;
@@ -32,8 +43,10 @@ router
     }
   });
 
-const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 3000 });
+console.log(
+  bold("🚀 Start listening on ") + green(`http://localhost:${PORT}`),
+);
+await app.listen({ port: PORT });
